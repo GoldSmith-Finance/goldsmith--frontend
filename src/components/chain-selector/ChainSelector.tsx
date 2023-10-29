c53c9a6909edf96b1dfe9eb123247b9f9129a3d7
lordmike007 <manujasan23@gmail.com>
2023-10-29 10:04:04 +0530

    feat: added rainbowkit, configured chains

--- src/components/chain-selector/ChainSelector.tsx
+++ src/components/chain-selector/ChainSelector.tsx
@@ -1,4 +1,5 @@
 import chains from 'src/chains/chains'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccountHook } from 'src/store/accountContext'
 
 const ChainSelector = () => {
@@ -6,17 +7,99 @@ const ChainSelector = () => {
 
   return (
     <div>
-      <select
-        id="chains"
-        onChange={(event) => setChainId(event.target.value as string)}
-        className="py-3 px-10 font-semibold font-poppins bg-[#ffc300] border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
-      >
-        {chains.map((chain) => (
-          <option value={chain.id} onClick={() => setChainId(chain.id)}>
-            {chain.label}
-          </option>
-        ))}
-      </select>
+      <ConnectButton.Custom>
+        {({
+          account,
+          chain,
+          openAccountModal,
+          openChainModal,
+          openConnectModal,
+          authenticationStatus,
+          mounted
+        }) => {
+          // Note: If your app doesn't use authentication, you
+          // can remove all 'authenticationStatus' checks
+          const ready = mounted && authenticationStatus !== 'loading'
+          const connected =
+            ready &&
+            account &&
+            chain &&
+            (!authenticationStatus || authenticationStatus === 'authenticated')
+
+          return (
+            <div
+              {...(!ready && {
+                'aria-hidden': true,
+                style: {
+                  opacity: 0,
+                  pointerEvents: 'none',
+                  userSelect: 'none'
+                }
+              })}
+            >
+              {(() => {
+                if (!connected) {
+                  return (
+                    <button
+                      className="py-3 px-10 font-semibold font-poppins bg-[#ffc300] border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                      onClick={openConnectModal}
+                      type="button"
+                    >
+                      Connect Wallet
+                    </button>
+                  )
+                }
+
+                if (chain.unsupported) {
+                  return (
+                    <button
+                      className="py-3 px-10 font-semibold font-poppins bg-[#ffc300] border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                      onClick={openChainModal}
+                      type="button"
+                    >
+                      Wrong network
+                    </button>
+                  )
+                }
+
+                return (
+                  <button
+                    className="py-3 px-4 font-semibold font-poppins bg-[#ffc300] border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+                    onClick={openChainModal}
+                    style={{ display: 'flex', gap: 12 }}
+                  >
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      {chain.hasIcon && (
+                        <div
+                          style={{
+                            background: chain.iconBackground,
+                            width: 12,
+                            height: 12,
+                            borderRadius: 999,
+                            overflow: 'hidden',
+                            marginRight: 4
+                          }}
+                        >
+                          {chain.iconUrl && (
+                            <img
+                              alt={chain.name ?? 'Chain icon'}
+                              src={chain.iconUrl}
+                              style={{ width: 12, height: 12 }}
+                            />
+                          )}
+                        </div>
+                      )}
+                      {chain.name}
+                    </div>
+
+                    <div>{account.displayName}</div>
+                  </button>
+                )
+              })()}
+            </div>
+          )
+        }}
+      </ConnectButton.Custom>
     </div>
   )
 }
--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,7 +1,12 @@
+import '@rainbow-me/rainbowkit/styles.css'
+import { Chain, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { AccountHookProvider } from 'src/store/accountContext'
-import { WagmiConfig, createConfig, mainnet } from 'wagmi'
-import { createPublicClient, http } from 'viem'
+import { WagmiConfig, configureChains, createConfig, mainnet } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { polygon, optimism, arbitrum } from 'viem/chains'
+
 declare module '@mui/material/styles' {
   interface BreakpointOverrides {
     xs: true
@@ -71,21 +76,78 @@ let theme = createTheme({
     }
   }
 })
-const config = createConfig({
-  autoConnect: true,
-  publicClient: createPublicClient({
-    chain: mainnet,
-    transport: http()
-  })
+
+const apothem: Chain = {
+  id: 51,
+  name: 'Apothem',
+  network: 'XDC',
+  iconUrl:
+    'https://static.wixstatic.com/media/9296c5_93002a44908b4edf93f7356fb9b62505~mv2.png/v1/fill/w_61,h_61,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/XDC.png',
+
+  iconBackground: '#fff',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'XDC',
+    symbol: 'TXDC'
+  },
+  rpcUrls: {
+    public: { http: ['https://rpc.apothem.network'] },
+    default: { http: ['https://rpc.apothem.network'] }
+  },
+  blockExplorers: {
+    default: { name: 'Apothem Explorer', url: 'https://explorer.apothem.network/' },
+    etherscan: { name: 'Apothem Explorer', url: 'https://explorer.apothem.network/' }
+  },
+  testnet: true
+}
+
+const wanchain_testnet: Chain = {
+  id: 999,
+  name: 'Wanchain Testnet',
+  network: 'Wanchain',
+  iconUrl:
+    'https://static.wixstatic.com/media/9296c5_baa08fd32afd43cdbc2da4c2932828de~mv2.png/v1/fill/w_61,h_61,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/WAN.png',
+  iconBackground: '#fff',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Wanchain',
+    symbol: 'WAN'
+  },
+  rpcUrls: {
+    public: { http: ['https://gwan-ssl.wandevs.org:46891'] },
+    default: { http: ['https://gwan-ssl.wandevs.org:46891'] }
+  },
+  blockExplorers: {
+    default: { name: 'WanScan', url: 'https://testnet.wanscan.org/' },
+    etherscan: { name: 'WanScan', url: 'https://testnet.wanscan.org/' }
+  },
+  testnet: true
+}
+
+const { chains, publicClient } = configureChains([apothem, wanchain_testnet], [publicProvider()])
+
+const { connectors } = getDefaultWallets({
+  appName: 'GoldSmith Finance',
+  projectId: '224a04c9b1c5a31a4ab2991c84b8ce4f',
+  chains
 })
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
+
 theme = createTheme(theme)
 
 const Providers = ({ children }: { children: JSX.Element }) => {
   const clientID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? ''
   return (
     <ThemeProvider theme={theme}>
-      <WagmiConfig config={config}>
-        <AccountHookProvider>{children}</AccountHookProvider>
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+          <AccountHookProvider>{children}</AccountHookProvider>
+        </RainbowKitProvider>
       </WagmiConfig>
     </ThemeProvider>
   )
